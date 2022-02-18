f835e639b86b483582d3a0defe08970599e30fa6
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-02-19 00:57:12 +0600

    Scrap success

--- Project 1/scrapper.py
+++ Project 1/scrapper.py
@@ -1,5 +1,7 @@
-import requests
-from bs4 import BeautifulSoup
+from unittest import result
+from urllib import response
+import requests as req
+from bs4 import BeautifulSoup as bs
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -7,4 +9,29 @@ import datetime
 
 now = datetime.datetime.now()
 
-print(now)
+#email content placeholder
+content = ''
+
+def info(url):
+  print("Parsing All Data")
+  item = ''
+  
+#Testing 
+
+response = req.get("https://baust.edu.bd/cse/employees/")
+data = response.content
+result = bs(data, 'html.parser')
+
+# print(result.prettify())
+
+names = result.find_all("h3", attrs={"class": "title"})
+ranks = result.find_all("h6", attrs={"class": "designation-title"})
+phones = result.find_all("span", attrs={"class": "designation-title"})
+print(ranks)
+
+for name,rank in zip(names,ranks):
+    data = [name.string.strip(), rank.string.strip()]
+    print(data)
+    # print(f'{name.string.strip()} = {rank.string.strip()}')  
+    # print(rank.string.strip())
+
---
+++ Project 1/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+for name,rank in zip(enumerate(names), enumerate(ranks)):
+#   # print(f'{name.string.strip()} = {rank.string.strip()}')  
+#   print(rank.string.strip())
